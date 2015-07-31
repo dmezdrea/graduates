5436f02f385c98844c763c044a770f6467da9ba4
dmezdrea <dumitru.mezdrea@endava.com>
2015-07-31 13:21:59 +0300

    initial commit

---
+++ src/test/java/com/endava/graduates/FirstTest.java
@@ -0,0 +1,14 @@
+package com.endava.graduates;
+
+import org.junit.Test;
+
+/**
+ * Created by dmezdrea on 7/31/2015.
+ */
+public class FirstTest {
+
+    @Test
+    public void test(){
+        System.out.println("Worksss!!");
+    }
+}
---
+++ src/test/java/com/endava/graduates/SecondTest.java
@@ -0,0 +1,15 @@
+package com.endava.graduates;
+
+import org.junit.Test;
+
+/**
+ * Created by dmezdrea on 7/31/2015.
+ */
+public class SecondTest {
+
+    @Test
+    public void test(){
+        String env = System.getProperty("env");
+        System.out.println(env);
+    }
+}
