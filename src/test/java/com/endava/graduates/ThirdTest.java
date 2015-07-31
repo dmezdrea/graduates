3521c7bbc6f921aef2dc775097903e7b1a0cac30
dmezdrea <dumitru.mezdrea@endava.com>
2015-07-31 15:16:30 +0300

    Adaugare ThirdTest

---
+++ src/test/java/com/endava/graduates/ThirdTest.java
@@ -0,0 +1,14 @@
+package com.endava.graduates;
+
+import org.junit.Test;
+
+/**
+ * Created by dmezdrea on 7/31/2015.
+ */
+public class ThirdTest {
+
+    @Test
+    public void test(){
+        System.out.println("Al 3-lea test");
+    }
+}
